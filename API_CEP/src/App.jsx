26fc950eb29a95fbcce211c3ca35c2e46e3198f9
robs-am <roberta.amaro89@gmail.com>
2022-10-25 20:04:08 -0300

    add condicional if and fetch

--- API_CEP/src/App.jsx
+++ API_CEP/src/App.jsx
@@ -1,3 +1,4 @@
+import { EventOutlined } from "@material-ui/icons";
 import { useState } from "react";
 
 import "./App.css";
@@ -8,10 +9,19 @@ function App() {
 
   const [endereco, setEndereco] = useState({});
 
+    function manipulaEndereco(evento) {
+      setEndereco({
+        cep: evento.target.value
+      })
+        if (endereco.cep && endereco.cep.length === 8) { // se endereço existir (&&) e tiver o tamanho(length) = 8
+          fetch('https://viacep.com.br/ws/01001000/json/')
+        }
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <input placeholder="Digite o cep" onChange={manipulaEndereço}/>
+        <input placeholder="Digite o cep" onChange={manipulaEndereco}/> //passa o onChange para dentro de uma função
       <ul>
         <li>CEP: {endereco.cep}</li>
       </ul>
