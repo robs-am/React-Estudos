ca3e53db000587b0894b6c9ffcf0ce903ea85979
robs-am <roberta.amaro89@gmail.com>
2022-10-25 17:57:08 -0300

    add useState and onChange event

--- API_CEP/src/App.jsx
+++ API_CEP/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 import "./App.css";
 
+const [cep, setCep] = useState("");
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input placeholder="Digite o cep" />
+        <input placeholder="Digite o cep" onChange={evento => setCep(evento.target.value)}/>
       </header>
     </div>
   );
