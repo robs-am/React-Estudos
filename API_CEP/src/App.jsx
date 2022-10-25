84eaf025e37f6639a92ef60dafbb3771bef0bb63
robs-am <roberta.amaro89@gmail.com>
2022-10-25 17:06:37 -0300

    first comit

---
+++ API_CEP/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input placeholder="Digite o cep" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
