e06bdc4dbc762547dc5e9f3dd01e6416dd9b938a
robs-am <roberta.amaro89@gmail.com>
2022-10-25 12:55:59 -0300

    troca de nome

--- useState-CEP/src/App.jsx
+++ useState-CEP/src/App.jsx
@@ -1,18 +1,23 @@
 
-
+import {useState} from "react";
 import './App.css'
 
 function App() {
   
+  const [nome, setNome] = useState("Roberta")
 
+  function alterarNome() {
+    setNome ("Roberta Amaro")
+  }
   return (
     <div className="App">
       
-        
+        <header>
         <h1>
-          aloow
+          {nome}
         </h1>
-      
+        <button onClick={alterarNome}>Mudar nome</button>
+        </header>
     </div>
   )
 }
