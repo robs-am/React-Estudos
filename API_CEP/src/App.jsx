b5bf95f64b8e1ab630ffa12c8e1b213a163a432e
robs-am <roberta.amaro89@gmail.com>
2022-10-25 22:27:49 -0300

    add cep funcionando

--- API_CEP/src/App.jsx
+++ API_CEP/src/App.jsx
@@ -1,42 +1,49 @@
-import { EventOutlined } from "@material-ui/icons";
 import { useState } from "react";
 
 import "./App.css";
 
-
-
 function App() {
-
   const [endereco, setEndereco] = useState({});
 
-    function manipulaEndereco(evento) {
-      setEndereco({
-        cep: evento.target.value
-      })
-        if (endereco.cep && endereco.cep.length === 8) { // se endereço existir (&&) e tiver o tamanho(length) = 8
-          fetch(`https://viacep.com.br/ws/${endereco.cep}/json/`) //retiramos o cep dos correios e substituimos por template string
-          .then(resposta => resposta.json()) //se a promessa der certo, teremos a resposta em json
-          .then(dados => {
-            setEndereco(dados{
-              "rua": dados.logradouro,
-              "bairro": dados.bairro,
-              "cidade": dados.localidade,
-              "estado":dados.uf,
+  function manipulaEndereco(evento) {
+    const cep = evento.target.value;
 
-            }
+    setEndereco({
+      cep,
+    });
 
-            );
-          })
-        }
+    //condicional e fetch API que buscará as informações do CEP//
+
+    if (cep && cep.length === 8) {
+      // se endereço existir (&&) e tiver o tamanho(length) = 8
+      fetch(`https://viacep.com.br/ws/${cep}/json/`) //retiramos o cep dos correios e substituimos por template string
+        .then((resposta) => resposta.json()) //se a promessa der certo, teremos a resposta em json
+        .then((dados) => {
+          setEndereco({
+            cep: cep,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf,
+          });
+        });
     }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <input placeholder="Digite o cep" onChange={manipulaEndereco}/> //passa o onChange para dentro de uma função
-      <ul>
-        <li>CEP: {endereco.cep}</li>
-      </ul>
+
+        
+        <input placeholder="Digite o cep" onChange={manipulaEndereco} />
+
+        <ul>
+          <li>CEP: {endereco.cep}</li>
+
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
       </header>
     </div>
   );
